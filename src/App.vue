f34fb942492cf87147a16b1c19d8fea20d3a2bba
Armand Marechal <sypenfu@gmail.com>
2023-02-09 14:46:08 +0100

    Update layout

--- src/App.vue
+++ src/App.vue
@@ -13,22 +13,46 @@ const handleLogout = async () => {
 </script>
 
 <template>
-  <header class="mx-auto max-w-6xl">
-    <div class="py-4 flex">
-      <div class="grow">
-        <RouterLink to="/" class="">Home</RouterLink>
-      </div>
+  <div class="mx-8 xl:mx-auto max-w-7xl">
+    <header>
+      <nav class="my-4 bg-white border-gray-200 rounded">
+        <div class="flex flex-wrap items-center justify-between mx-auto">
+          <RouterLink to="/" class="flex items-center">
+            <span class="text-xl font-semibold whitespace-nowrap">
+              Study Hub
+            </span>
+          </RouterLink>
 
-      <RouterLink v-if="!user" to="/login" class="">Login</RouterLink>
-      <button
-        v-else
-        @click="handleLogout()"
-        class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-      >
-        Logout
-      </button>
-    </div>
-  </header>
+          <!-- Links -->
+          <div>
+            <ul class="flex gap-2">
+              <li>
+                <RouterLink to="/" class="py-2 px-4 text-gray-700 rounded bg-gray-100 hover:bg-gray-200">
+                  Home
+                </RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/" class="py-2 px-4 text-gray-700 rounded bg-gray-100 hover:bg-gray-200">
+                  Other
+                </RouterLink>
+              </li>
+            </ul>
+          </div>
 
-  <RouterView />
+          <!-- Action -->
+          <div class="flex">
+            <RouterLink v-if="!user" to="/login" class="py-2 px-4 text-white bg-blue-600 hover:bg-blue-700 rounded">
+              Login
+            </RouterLink>
+            <button v-else @click="handleLogout()" type="button"
+              class="py-2 px-4 text-white bg-gray-800 hover:bg-gray-900 rounded">
+              Logout
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <RouterView />
+  </div>
 </template>
